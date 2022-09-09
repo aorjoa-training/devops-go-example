1b719f744d2ca0ec3855f982d68ff59800aea844
bhuridech <aorjoa.training@gmail.com>
2022-09-09 15:23:33 +0700

    change to hello world

--- rest/handler/handler.go
+++ rest/handler/handler.go
@@ -17,7 +17,7 @@ func NewApplication(db *sql.DB) *handler {
 }
 
 func (h *handler) Greeting(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World! AorJoa")
+	return c.String(http.StatusOK, "Hello, World!")
 }
 
 type NewsArticle struct {
